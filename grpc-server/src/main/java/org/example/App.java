ffc3359753eb5dba1ec512d3486fada232ceceaf
Артур <fazlyev.art@gmail.com>
2021-06-22 23:21:12 +0500

    initial commit

--- grpc-server/src/main/java/org/example/App.java
+++ grpc-server/src/main/java/org/example/App.java
@@ -1,4 +1,18 @@
 package org.example;
 
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
 public class App {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Server server = ServerBuilder
+                .forPort(8080)
+                .addService(new GreetingServiceImpl())
+                .build();
+
+        server.start();
+        server.awaitTermination();
+    }
 }
