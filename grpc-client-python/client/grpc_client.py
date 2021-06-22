0fbb561a0acc6b738a762daa1930147d28adccac
Артур <fazlyev.art@gmail.com>
2021-06-23 00:15:56 +0500

    initial commit

--- grpc-client-python/client/grpc_client.py
+++ grpc-client-python/client/grpc_client.py
@@ -6,8 +6,8 @@ from proto import greeting_service_pb2_grpc, greeting_service_pb2
 def run():
     with grpc.insecure_channel("localhost:8080") as channel:
         stub = greeting_service_pb2_grpc.GreetingServiceStub(channel)
-        response = stub.greeting(greeting_service_pb2.HelloRequest(name='test'))
-        print(f"Greeting from server: " + response.greeting)
+        for response in stub.greeting(greeting_service_pb2.HelloRequest(name='test')):
+            print(f"Greeting from server: " + response.greeting)
 
 
 if __name__ == '__main__':
